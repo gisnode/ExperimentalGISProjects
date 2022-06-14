892b38d776bd4f0f3f57c439b45f23e5aeaf4b4c
wezru <rukmangadanies@gmail.com>
2022-06-14 11:21:12 +0530

    p

--- exifr/src/App.vue
+++ exifr/src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div id="approot">
-    
+    <button>Images Dir</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ref } from '@vue/reactivity';
+import './App.scss';
+
+export default defineComponent({
+  setup() {
+    const imagesdir = ref('');
+    const csvpath = ref('');
+    const targetdir = ref('');
+
+    return {
+      imagesdir, csvpath, targetdir
+    }
+
+  },
+})
+</script>
