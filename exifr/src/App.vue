8328167b0572570d5e1a820309a1bae08afb0f8c
wezru <rukmangadanies@gmail.com>
2022-06-14 11:47:32 +0530

    p

--- exifr/src/App.vue
+++ exifr/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="approot">
-    <button class="inptbnt" v-on:click="selectimagesdir">Select Images Folder</button>
+    <div style="font-size:20px;">EXIFR</div><br/>
+    <button class="inputbtn" v-on:click="selectimagesdir">Select Images Folder</button>
     <div class="clientmsg">{{ imagesdir }}</div>
-    <button class="inptbnt" v-on:click="selectcsvfile">Select CSV File</button>
+    <button class="inputbtn" v-on:click="selectcsvfile">Select CSV File</button>
     <div class="clientmsg">{{ csvpath }}</div>
-    <button class="inptbnt" v-on:click="selectoutdir">Select Output Folder</button>
+    <input type="checkbox" v-model="hasHeader">
+    <span>Has Header</span><br/>
+    <button class="inputbtn" v-on:click="selectoutdir">Select Output Folder</button>
     <div class="clientmsg">{{ outdir }}</div>
   </div>
 </template>
@@ -18,9 +21,11 @@ import { ipcRenderer } from 'electron';
 
 export default defineComponent({
   setup() {
-    const imagesdir = ref('');
-    const csvpath = ref('');
-    const outdir = ref('');
+    const imagesdir = ref('X://folder');
+    const csvpath = ref('X://folder/csvfile');
+    const outdir = ref('Y://folde21');
+
+    const hasHeader = ref(true);
 
     const selectimagesdir = () => {
       ipcRenderer.send('open-folder', ['Select Images Folder', 'imagesfolder']);
@@ -34,9 +39,22 @@ export default defineComponent({
       ipcRenderer.send('open-folder', ['Select Output Folder', 'outputfolder']);
     }
 
+    ipcRenderer.on('imagesfolder', (event, arg) => {
+      imagesdir.value = arg;
+    });
+
+    ipcRenderer.on('csvfile', (event, arg) => {
+      csvpath.value = arg;
+    });
+
+    ipcRenderer.on('outputfolder', (event, arg) => {
+      outdir.value = arg;
+    });
+
     return {
       imagesdir, csvpath, outdir,
-      selectimagesdir, selectcsvfile, selectoutdir
+      selectimagesdir, selectcsvfile, selectoutdir,
+      hasHeader
     }
 
   },
