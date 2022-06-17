b44f3dc3a80e0931fe2048f9261b80ddb8436e51
wezru <rukmangadanies@gmail.com>
2022-06-17 10:09:56 +0530

    p

--- testbinembed/src/App.vue
+++ testbinembed/src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="approot">
+    Hello App
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  setup() {
+    
+  },
+})
 </script>
+
+
 
 <style>
 #app {
