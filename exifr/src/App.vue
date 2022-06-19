0c519568ca4387dc8f17942c28d9367ea2b39c4d
wezru <rukmangadanies@gmail.com>
2022-06-19 18:33:26 +0530

    Update App.vue

--- exifr/src/App.vue
+++ exifr/src/App.vue
@@ -7,8 +7,8 @@
         <td><button class="inputbtn" v-on:click="selectcsvfile">Select CSV File</button></td>
       </tr>
       <tr>
-        <td><span>{{ imagesdir }}</span></td>
-        <td><span>{{ csvpath }}</span></td>
+        <td><span>{{ imagesdirdisplay }}</span></td>
+        <td><span>{{ csvpathdisplay }}</span></td>
       </tr>
     </table>
 
@@ -66,17 +66,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { ref } from '@vue/reactivity';
 import './App.scss';
 
-import { ipcRenderer } from 'electron';
+import fs from 'fs';
 import path from 'path';
+
+import { ipcRenderer } from 'electron';
+import { parse } from 'csv-parse/sync';
 
 export default defineComponent({
   setup() {
-    const imagesdir = ref('X://folder');
-    const csvpath = ref('Y://file.csv');
+    const imagesdir = ref('');
+    const csvpath = ref('');
+
+    const imagesdirdisplay = ref('X://folder');
+    const csvpathdisplay = ref('Y://file.csv');
 
     const defaultMsg = 'Click XIF to Start';
     const statusmsg = ref(defaultMsg);
@@ -89,7 +95,16 @@ export default defineComponent({
     const longitudecolumn = ref();
     const altitudeecolumn = ref();
 
-    const columnList = ref(['1', '2', '3', '4']);
+    // const columnList = ref(['1', '2', '3', '4']);
+    const columnList = computed(() => {
+      setSomeColumns();
+
+      if (hasHeader.value){
+        return ['4', '3', '2', '1']
+      } else {
+        return ['1', '2', '3', '4']
+      }
+    });
 
     const csvParams = {
       hasHeader, csvLoaded,
@@ -112,29 +127,51 @@ export default defineComponent({
     }
 
     ipcRenderer.on('imagesfolder', (event, arg) => {
+      imagesdir.value = arg;
+      
       let foldername = path.basename(path.dirname(arg));
-      imagesdir.value = foldername.length < 10 ? foldername : foldername.substring(0, 10) + '...';
+      imagesdirdisplay.value = foldername.length < 10 ? foldername : foldername.substring(0, 10) + '...';
     });
 
     ipcRenderer.on('csvfile', (event, arg) => {
       if(path.extname(arg) == '.csv'){
         let filename = path.basename(arg, '.csv');
-        csvpath.value = filename.length < 10 ? filename + '.csv' : filename.substring(0, 10) + '.. .csv';
+        csvpathdisplay.value = filename.length < 10 ? filename + '.csv' : filename.substring(0, 10) + '.. .csv';
 
-        csvActions();
+        csvActions(arg);
       } else {
         showTempMsg('Only CSVs', 2);
       }
     });
 
-    const csvActions = () => {
-      csvLoaded.value = true;
-      hasHeader.value = false;
+    const csvContentParse = ref();
 
+    const readCSVNLoad = () => {
+      const csvContent = fs.readFileSync(csvpath.value, 'utf-8');
+      // console.log(csvContent);
+
+      const csvParsed = parse(csvContent, {
+        delimiter: ','
+      });
+
+      console.log(csvParsed);
+      csvContentParse.value = csvParsed;
+    }
+
+    const setSomeColumns = () => {
       imgnamecolumn.value = '1';
       latitudecolumn.value = '2';
       longitudecolumn.value = '3';
       altitudeecolumn.value = '4';
+    }
+
+    const csvActions = (arg: any) => {
+      csvpath.value = arg;
+      csvLoaded.value = true;
+      hasHeader.value = false;
+
+      readCSVNLoad();
+      setSomeColumns();
     }
 
     const startexifing = () => {
@@ -143,7 +180,7 @@ export default defineComponent({
     }
 
     return {
-      statusmsg, imagesdir, csvpath,
+      statusmsg, imagesdirdisplay, csvpathdisplay,
       selectimagesdir, selectcsvfile,
       ...csvParams,
       startexifing
