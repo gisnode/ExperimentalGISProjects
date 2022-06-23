28dc0d5697bb02a6a5886e1828dc6edbe7bab5c4
wezru <rukmangadanies@gmail.com>
2022-06-23 15:38:14 +0530

    p

---
+++ slitr/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="approot">
+    <button></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './App.scss';
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
