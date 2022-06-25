b124fd4379c6d3d4832de47b19069941e198a35d
wezru <rukmangadanies@gmail.com>
2022-06-25 09:53:57 +0530

    p

---
+++ geophotos-cataloguer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="approot">
+    jack
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
